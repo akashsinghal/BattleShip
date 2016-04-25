d20ffbd7811b2e62cb66fb0233a9e23cd9ef8a7e
shlokgharia <purpleanubis7@gmail.com>
2016-04-24 18:04:20 -0700

    Added the hitOrMiss method to GameLogic
    I changed the signature of the gamelogic class

--- battleshipserver/GameLogic.java
+++ battleshipserver/GameLogic.java
@@ -7,16 +7,35 @@
  */
 public class GameLogic
 {
-   static int x;
-   static int y;
-   public GameLogic (int xpos, int ypos)
-   {
-       x=xpos;
-       y=ypos;
+    static int x;
+    static int y;
+
+    static SheetOfButtons playerBoard;
+    static SheetOfButtons checkBoard;
+    public GameLogic ()
+    {
+        //x=xpos;
+        //y=ypos;
     }
-   public boolean isTaken()
-   {
-       return true; 
+
+    public boolean isTaken()
+    {
+        return true; 
     }
-   //hello
+
+    public boolean hitOrMiss(int xPos, int yPos)
+    {
+        x=xPos;
+        y=yPos;
+        if(checkBoard.getSpecificBoard(xPos,yPos).getBackground()== Color.BLACK)
+        {
+            return true;
+        }
+        else if(checkBoard.getSpecificBoard(xPos,yPos).getBackground()== Color.BLUE)
+        {
+            return false;
+        }
+        return false;
+    }
+    //hello
 }
